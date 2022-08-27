a3b48090aa86cb663856a57c5ed0325715904a60
atitov <alexey.titov@axelmwd.com>
2022-08-27 23:05:06 +0300

    - Simple moving Sorter.cs to PodkolzzzinVersion/Sorter.cs

--- Stream.Profiling/PodkolzzzinVersion/Sorter.cs
+++ Stream.Profiling/PodkolzzzinVersion/Sorter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace Stream.Profiling
+namespace Stream.Profiling.PodkolzzzinVersion
 {
     struct Line : IComparable<Line>
     {
