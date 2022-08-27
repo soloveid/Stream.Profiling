a48b9e147a3d52318bfa9ffe605893cf66f5616e
atitov <alexey.titov@axelmwd.com>
2022-08-27 23:23:15 +0300

    - Добавлена моя первая версия - Основные особенности == Line -- не хранит подстроки типа Word и Number, хранится только оригинальная целиковая строка (избегаем создания двух новых строк и при записи в результирующий файл создания полной строки) -- попытка в Compare сравнивать первые символы, перед тем как сравнивать все слова целиком (наверное глупо, так как обычный Compare так же просто идёт по элементам и сранивает) -- сравнение Number как строк == PartsSorter -- Заведение одного массива Line[] в который подгружаем данные для сортировки каждого batch-а (избегаем излишних new Line()) == PartsMerger -- Так же избегаем new Line() для каждой строки из файла

---
+++ Stream.Profiling/MyVersion01/PartsMerger.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Stream.Profiling.MyVersion01
+{
+    class PartsMerger
+    {
+        class FilePart : IComparable<FilePart>
+        {
+            public StreamReader Stream { get; }
+
+            public Line Line;
+
+            public string TotalLine
+            {
+                get { return Line.TotalLine; }
+            }
+
+            public FilePart(StreamReader stream, Line line)
+            {
+                Stream = stream;
+                Line = line;
+            }
+
+            public int CompareTo(FilePart other)
+            {
+                return Line.CompareTo(other.Line);
+            }
+
+            public void Update(string line)
+            {
+                Line.Update(line);
+            }
+        }
+
+        readonly string resultFileName;
+
+        readonly List<string> partFileNames;
+
+        public PartsMerger(string resultFileName, List<string> partFileNames)
+        {
+            this.resultFileName = resultFileName;
+            this.partFileNames = partFileNames;
+        }
+
+        public void Merge()
+        {
+            var partFiles = new FilePart[partFileNames.Count];
+            for (var i = 0; i < partFiles.Length; i++)
+            {
+                var streamReader = new StreamReader(partFileNames[i]);
+                var line = new Line();
+                line.Update(streamReader.ReadLine());
+                partFiles[i] = new FilePart(streamReader, line);
+            }
+
+            Array.Sort(partFiles, 0, partFiles.Length);
+
+            using (var streamWriter = new StreamWriter(resultFileName))
+            {
+                var firstPartIndex = 0;
+                while (firstPartIndex < partFiles.Length)
+                {
+                    var firstPartFile = partFiles[firstPartIndex];
+                    streamWriter.WriteLine(firstPartFile.Line.TotalLine);
+
+                    var readedLine = firstPartFile.Stream.ReadLine();
+                    if (readedLine != null)
+                    {
+                        partFiles[firstPartIndex].Line.Update(readedLine);
+                        for (var i = firstPartIndex; i < partFiles.Length - 1; i++)
+                        {
+                            if (partFiles[i].Line.CompareTo(partFiles[i + 1].Line) > 0)
+                            {
+                                var temp = partFiles[i];
+                                partFiles[i] = partFiles[i + 1];
+                                partFiles[i + 1] = temp;
+                                continue;
+                            }
+                            break;
+                        }
+                    }
+                    else
+                        firstPartIndex++;
+                }
+            }
+
+            for (var i = 0; i < partFiles.Length; i++)
+                partFiles[i].Stream.Dispose();
+        }
+    }
+}
---
+++ Stream.Profiling/MyVersion01/PartsSorter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Stream.Profiling.MyVersion01
+{
+    class PartsSorter
+    {
+        readonly string fileName;
+
+        readonly int partLinesCount;
+
+        readonly Line[] batchLines;
+
+        public PartsSorter(string fileName, int partLinesCount)
+        {
+            this.fileName = fileName;
+            this.partLinesCount = partLinesCount;
+            batchLines = new Line[partLinesCount];
+            for (var i = 0; i < batchLines.Length; i++)
+                batchLines[i] = new Line();
+        }
+
+        int GetBatch(StreamReader reader)
+        {
+            var batchIndex = 0;
+            for (; batchIndex < partLinesCount; batchIndex++)
+            {
+                var line = reader.ReadLine();
+                if (line != null)
+                {
+                    batchLines[batchIndex].Update(line);
+                    continue;
+                }
+
+                break;
+            }
+
+            return batchIndex;
+        }
+
+        public List<string> Sort()
+        {
+            var partFileNames = new List<string>();
+
+            using (var reader = new StreamReader(fileName))
+            {
+                for (var batchIndex = 0;; batchIndex++)
+                {
+                    var batchSize = GetBatch(reader);
+                    if (batchSize == 0)
+                        break;
+
+                    Array.Sort(batchLines, 0, batchSize);
+
+                    var partFileName = $"my1_{batchIndex + 1}.txt";
+                    using (var writer = new StreamWriter(partFileName))
+                    {
+                        for (var i = 0; i < batchLines.Length && i < batchSize; i++)
+                            writer.WriteLine(batchLines[i].TotalLine);
+                    }
+
+                    partFileNames.Add(partFileName);
+                }
+            }
+
+            return partFileNames;
+        }
+    }
+}
